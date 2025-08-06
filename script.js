110bb92dd64b961e324686f427f5568b9ebfb3db
AQUAMAN-OF-OYO <98422740+AQUAMAN-OF-OYO@users.noreply.github.com>
2025-08-06 19:33:48 +0100

    update: made changes to files

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById('menu-toggle');
+    const nav = document.getElementById('nav');
+
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+
+    // Hero Slider
+    let index = 0;
+    const slides = document.getElementById("slides");
+    const total = slides.children.length;
+
+    setInterval(() => {
+      index = (index + 1) % total;
+      slides.style.transform = `translateX(-${index * 100}vw)`;
+    }, 7000);
